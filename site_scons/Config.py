1f17bb1c4a04476ee8e06a35043a9f37d0dd8d15
Alex Scarborough <alex@gameclay.com>
2009-11-13 15:34:55 -0800

    Custom Build method now handles Glob() inputs and sub directories.

--- site_scons/Config.py
+++ site_scons/Config.py
@@ -1,4 +1,5 @@
 from SCons.Script import *
+from os import path
 
 class Config:
    allConfigs = {}
@@ -9,7 +10,7 @@ class Config:
       self.suffix = suffix
       Config.allConfigs[self.name] = self
       if default:
-      	Config.default = self.name      	
+         Config.default = self.name
 
    def modenv(self, env):
       newEnv = env.Clone()
@@ -20,30 +21,47 @@ def Build(environ, command, target, source, *args, **kw):
    configs = environ.subst('${cfg}').split()
    fullPath = {}
    cleanName = {}
+
+   # Build each config
    for config in configs:
       config = Config.allConfigs[config]
+
+      # Insert the configuration options
+      newEnv = config.modenv(environ)
+
       cleanDict = {}
+      # Do config substitution on incoming construction variables
       for key in kw.keys():
          if key.endswith('_CFG'):
            cleanDict[key[:-4]] = kw[key][config.name]
          else:
-         	cleanDict[key] = kw[key]
+            cleanDict[key] = kw[key]
+
       outdir = 'build/' + config.name + '/'
       newFiles = []
       if SCons.Util.is_String(source):
          source = [source]
+
+      # Tell SCons to build the files in a build/<cfg> directory
+      dirs = []
       for file in source:
+         dir, file = path.split(str(file))
          newFiles.append(outdir + file)
-      newEnv = config.modenv(environ)
+         if not dir:
+            dir = '.'
+         if not dir in dirs:
+         	dirs.append(dir)
+
+      for dir in dirs:
+         newEnv.VariantDir(outdir, dir, duplicate=0)
       newTarget = newEnv['BUILDERS'][command].get_prefix(newEnv) + target + config.suffix + newEnv['BUILDERS'][command].get_suffix(newEnv)
-      newEnv.VariantDir(outdir, '.', duplicate=0)
       newEnv.DoUniversal(command, outdir + newTarget, newFiles, *args, **cleanDict)
       fullPath[config.name] = newEnv.Command(newTarget, outdir + newTarget, Copy('$TARGET', '$SOURCE'))
       cleanName[config.name] = target +config.suffix
-   
+
    return fullPath, cleanName
-   
+
 def GetVars(vars):
 	vars.Add(ListVariable('cfg', 'Build configurations', Config.default, Config.allConfigs.keys()))
-   
+
 AddMethod(Environment, Build)
