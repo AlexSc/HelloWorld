f5c7e30ac4ac4fe9f28a98ed9b3e851e1ed1db15
Alex Scarborough <alex@gameclay.com>
2009-11-16 22:08:50 -0800

    The proper plugin name is now substitued into hello.def.  Ugly, but working

--- site_scons/Config.py
+++ site_scons/Config.py
@@ -19,7 +19,7 @@ class Config:
       newEnv.Prepend(**self.opts)
       return newEnv
 
-def Build(environ, command, target, source, copy=True, *args, **kw):
+def Build(environ, command, target, source, copy=True, suffix=True, *args, **kw):
    if environ['PLATFORM'] == 'win32':
       ld = SCons.Tool.createLoadableModuleBuilder(environ)
       ld.add_src_builder('RES')
@@ -59,25 +59,23 @@ def Build(environ, command, target, source, copy=True, *args, **kw):
 
       # Tell SCons to build the files in a build/<cfg> directory
       dirs = []
-      newSource = []
       for file in source:
          if SCons.Util.is_Dict(file):
-            newSource.append(file[config.name])
-         else:
-            newSource.append(file)
+            file = file[config.name]
          dir, file = path.split(str(file))
          newFiles.append(outdir + file)
          if not dir:
             dir = '.'
-         if not dir in dirs:
+         if not dir in dirs and not path.normpath(dir) == path.normpath(outdir):
             dirs.append(dir)
-
-      source = newSource
 
       for dir in dirs:
          newEnv.VariantDir(outdir, dir, duplicate=0)
 
-      newTarget = target + config.suffix
+      if suffix:
+         newTarget = target + config.suffix
+      else:
+         newTarget = target
       p = newEnv.DoUniversal(command, outdir + newTarget, newFiles, *args, 
                              **cleanDict)
 
@@ -87,7 +85,7 @@ def Build(environ, command, target, source, copy=True, *args, **kw):
          fullPath[config.name] = newEnv.Command(path.split(str(p[0]))[1], p,
                                                 Copy('$TARGET', '$SOURCE'))
       else:
-         fullPath[config.name] = p
+         fullPath[config.name] = p[0]
 
       # Save the unadorned name of the product to pass into other build comamnds
       cleanName[config.name] = newTarget
